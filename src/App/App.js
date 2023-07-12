3bb62c7e1b8af1f6197fa490435ac2b67308f74c
Theojeddy <Theojeddy@hotmail.co.uk>
2023-07-12 12:58:44 +0100

    back to square 1

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weightlifting Rep Calculator</h1>
+        <p>This is a work in progress. Please check back later.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
