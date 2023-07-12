bab3106f2eb09b6a37c79d91cb11a63e0e5069e2
Theojeddy <Theojeddy@hotmail.co.uk>
2023-07-12 14:42:55 +0100

    ReadMe updated and logo created adn added.

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-
+import logo1 from './RepWise-logo.svg'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img
+          src={logo1}
+          alt="Logo"
+          className="logo-image"
+          
+        />
         <h1>Weightlifting Rep Calculator</h1>
         <p>This is a work in progress. Please check back later.</p>
       </header>
