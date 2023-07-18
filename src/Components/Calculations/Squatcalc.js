42452b5d324bdb584e75b1e0c556654dfc6c89b5
Theojeddy <Theojeddy@hotmail.co.uk>
2023-07-18 13:48:27 +0100

    toggle added to cards / list view

--- src/Components/Calculations/Squatcalc.js
+++ src/Components/Calculations/Squatcalc.js
@@ -1,22 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 
 export default function SquatCalc({ completed }) {
-  // Destructure the 'completed' prop
   const exercise = completed.exercise;
   const weight = completed.weight;
   const reps = completed.reps;
 
-  // Function to generate the training list
   const generateTrainingList = () => {
     const trainingList = [];
 
-    // Loop through rep ranges from 1 to 10
     for (let i = 1; i <= 10; i++) {
       let percentage;
       let currentWeight;
 
-      // Calculate percentage adjustment based on rep range
       if (i < reps) {
         percentage = 100 + (reps - i) * 2;
       } else if (i > reps) {
@@ -25,10 +21,8 @@ export default function SquatCalc({ completed }) {
         percentage = 100;
       }
 
-      // Calculate the weight for the current rep range
       currentWeight = Math.round((weight * percentage) / 100);
 
-      // Add the rep range and weight to the training list
       trainingList.push({
         reps: i,
         weight: currentWeight,
@@ -38,26 +32,45 @@ export default function SquatCalc({ completed }) {
     return trainingList;
   };
 
-  // Generate the training list
   const trainingList = generateTrainingList();
 
-  // Render the component
+  const [viewMode, setViewMode] = useState("cards");
+
+  const toggleViewMode = () => {
+    setViewMode((prevMode) => (prevMode === "cards" ? "list" : "cards"));
+  };
+
   return (
     <div className="text-center">
       <h1>Exercise: {exercise}</h1>
-      <h1>Weight: {weight}</h1>
+      <h1>Weight: {weight} kg</h1>
       <h1>Reps: {reps}</h1>
       <h2>Training List:</h2>
-      <div className="d-flex flex-wrap justify-content-center">
-        {trainingList.map((item, index) => (
-          <Card key={index} style={{ width: "18rem" }} className="m-2">
-            <Card.Body>
-              <Card.Title>Reps: {item.reps}</Card.Title>
-              <Card.Text>Weight: {item.weight}kg</Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
+      <div className="d-flex justify-content-center mb-3">
+        <button className="btn btn-primary" onClick={toggleViewMode}>
+          Toggle View
+        </button>
       </div>
+      {viewMode === "cards" ? (
+        <div className="d-flex flex-wrap justify-content-center">
+          {trainingList.map((item, index) => (
+            <Card key={index} style={{ width: "18rem" }} className="m-2">
+              <Card.Body>
+                <Card.Title>Reps: {item.reps}</Card.Title>
+                <Card.Text>Weight: {item.weight}kg</Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        <ul className="list-group">
+          {trainingList.map((item, index) => (
+            <li key={index} className="list-group-item">
+              Reps: {item.reps}, Weight: {item.weight}kg
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
