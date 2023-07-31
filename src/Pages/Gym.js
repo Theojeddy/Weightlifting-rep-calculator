bc6318f1412be75f29f8a7c1c02f7270c2b2d10a
Theojeddy <Theojeddy@hotmail.co.uk>
2023-07-31 19:19:23 +0100

    api call being made, console logs, need to allow to inputs of address/location and to present the map with gyms on

--- src/Pages/Gym.js
+++ src/Pages/Gym.js
@@ -13,6 +13,21 @@ export default function GymLocator({ title, setTitle }) {
   useEffect(() => {
     setTitle("Gym locator");
   }, [setTitle]);
+
+  
+
+
+async function fetchGyms() {
+    const response = await fetch('https://api.geoapify.com/v2/places?categories=sport.fitness,sport.sports_centre&filter=circle:-2.0816813,52.5110832,2000&bias=proximity:-2.0816813,52.5110832&lang=en&limit=20&apiKey=3d1509c62983467a8c576bdff4fc9aa3');
+    const data = await response.json();      
+
+console.log(data);
+console.log(data.features[0].properties.name);
+console.log(data.features[1].properties.name);
+console.log(data.features[2].properties.name);
+console.log(data.features[3].properties.name);
+    
+  }
   
   return (
     <div className="container-fluid vw4">
@@ -31,6 +46,9 @@ export default function GymLocator({ title, setTitle }) {
         <div className="col mt-4 text-end">
           <HeaderHomeButton />
         </div>
+      </div>
+      <div className="row mt-3">
+        <button onClick={fetchGyms}> search </button>
       </div>
       
 
