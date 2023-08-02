f6b33816241b3252ffcad613afd7c22d84eff030
Theojeddy <Theojeddy@hotmail.co.uk>
2023-08-02 13:38:15 +0100

    added input bar, usestate and error, 1 warning error not being used

--- src/Pages/Gym.js
+++ src/Pages/Gym.js
@@ -5,10 +5,11 @@ import HeaderLogo from "../Components/Header/HeaderLogo";
 import HeaderHomeButton from "../Components/Header/HomeButton";
 
 // import useEffect and useState
-import React, { useEffect,} from "react";
+import React, { useState, useEffect,} from "react";
 
 export default function GymLocator({ title, setTitle }) {
- 
+ const [address, setAddress] =useState ("")
+ const [error, setError] = useState(null);
   //set up useEffect to change the title to calculator
   useEffect(() => {
     setTitle("Gym locator");
@@ -18,16 +19,35 @@ export default function GymLocator({ title, setTitle }) {
 
 
 async function fetchGyms() {
-    const response = await fetch('https://api.geoapify.com/v2/places?categories=sport.fitness,sport.sports_centre&filter=circle:-2.0816813,52.5110832,2000&bias=proximity:-2.0816813,52.5110832&lang=en&limit=20&apiKey=3d1509c62983467a8c576bdff4fc9aa3');
-    const data = await response.json();      
-
-console.log(data);
-console.log(data.features[0].properties.name);
-console.log(data.features[1].properties.name);
-console.log(data.features[2].properties.name);
-console.log(data.features[3].properties.name);
-    
+  setError(null);
+  if (address === "") {
+    setError("Please enter a valid address");
+    return;
   }
+  try {
+    const response = await fetch(
+      `https://api.geoapify.com/v2/places?categories=sport.fitness,sport.sports_centre&filter=circle:-2.11334,52.54686,5000&bias=proximity:-2.11334,52.54686&lang=en&limit=20&apiKey=3d1509c62983467a8c576bdff4fc9aa3`
+    );
+    const data = await response.json();
+
+    console.log(data);
+
+    if (!response.ok) {
+      setError("Something went wrong, try again");
+      return;
+    }
+
+    console.log(data);
+    console.log(data.features[0].properties.name);
+    console.log(data.features[1].properties.name);
+    console.log(data.features[2].properties.name);
+    console.log(data.features[3].properties.name);
+  } catch (error) {
+    // Handle errors that might occur during fetch or response parsing
+    setError("An error occurred. Please try again later.");
+  }
+}
+
   
   return (
     <div className="container-fluid vw4">
@@ -48,10 +68,18 @@ console.log(data.features[3].properties.name);
         </div>
       </div>
       <div className="row mt-3">
-        <button onClick={fetchGyms}> search </button>
+        <input
+          type="text"
+          placeholder="Enter location"
+          onChange={(e) => setAddress(e.target.value)}
+        />
+          
+        
+        <button className="btn btn-primary" onClick={fetchGyms}>
+          {" "}
+          search{" "}
+        </button>
       </div>
-      
-
     </div>
   );
 }
