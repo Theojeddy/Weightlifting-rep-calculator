168230a919584cf38baa428f6ae27e8251b10ec2
Theojeddy <Theojeddy@hotmail.co.uk>
2023-08-22 14:42:54 +0100

    logo now a home button

--- src/Components/Header/HeaderLogo.js
+++ src/Components/Header/HeaderLogo.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
 import logo1 from "../../App/RepWise-logo.svg";
 
 import "bootstrap/dist/css/bootstrap.css";
 
+
 export default function HeaderLogo(){
 return (
-  
+  <Link to="/">
     <header>
       <img src={logo1} alt="Logo" className="img-fluid" />
     </header>
-    
+  </Link>
 );};
--- src/Pages/QuickWorkouts.js
+++ src/Pages/QuickWorkouts.js
@@ -1,16 +1,18 @@
 import HeaderLogo from "../Components/Header/HeaderLogo";
 import React, { useEffect } from "react";
 
+
 export default function QuickWorkouts({ title, setTitle }) {
   useEffect(() => {
-    setTitle("Home");
+    setTitle("Quick Workouts");
   }, [setTitle]);
   return (
     <div className="container-fluid vw4">
       <div className="row">
         <div className="col mt-4 text-start">
           <div style={{ width: "15vw" }}>
-            <HeaderLogo />
+              <HeaderLogo />
+            
           </div>
         </div>
 
