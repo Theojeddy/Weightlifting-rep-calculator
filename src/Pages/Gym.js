47a807e2ff65ffafb776ebd7ca30111f926e2c88
Theojeddy <Theojeddy@hotmail.co.uk>
2023-08-22 13:01:53 +0100

    gym locator error fixed

--- src/Pages/Gym.js
+++ src/Pages/Gym.js
@@ -1,15 +1,13 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Card } from "react-bootstrap";
 import HeaderLogo from "../Components/Header/HeaderLogo";
 import HeaderHomeButton from "../Components/Header/HomeButton";
 
-import React, { useState, useEffect } from "react";
-
 export default function GymLocator({ title, setTitle }) {
   const [address, setAddress] = useState("");
   const [error, setError] = useState(null);
   const [gymData, setGymData] = useState([]);
-  
 
   useEffect(() => {
     setTitle("Gym locator");
@@ -24,12 +22,9 @@ export default function GymLocator({ title, setTitle }) {
 
     try {
       // Geocode the address to obtain latitude and longitude
-      const geocodeUrl = `https://nominatim.openstreetmap.org/?addressdetails=1&
-      q=${encodeURIComponent(
+      const geocodeUrl = `https://nominatim.openstreetmap.org/?addressdetails=1&q=${encodeURIComponent(
         address
       )}&format=json&limit=1`;
-      
-      
 
       const geocodeResponse = await axios.get(geocodeUrl);
       if (geocodeResponse.data.length === 0) {
@@ -38,23 +33,22 @@ export default function GymLocator({ title, setTitle }) {
       }
       const { lat, lon } = geocodeResponse.data[0];
 
-      // Use the obtained coordinates in the API request
+      // Use axios for the API request
       const url = `https://api.geoapify.com/v2/places?categories=sport.fitness,sport.sports_centre&filter=circle:${lon},${lat},5000&bias=proximity:${lon},${lat}&lang=en&limit=20&apiKey=3d1509c62983467a8c576bdff4fc9aa3`;
 
-      const response = await fetch(url);
-      const data = await response.json();
+      const response = await axios.get(url);
+      const data = response.data;
 
-      if (!response.ok) {
+      if (response.status < 200 || response.status >= 300) {
         setError("Sorry, something went wrong, please try again");
         return;
       }
 
-      setGymData(data.features.slice(0, 20)); // Update gymData with the first 20 elements of the data array
+      setGymData(data.features.slice(0, 20));
     } catch (error) {
       setError("An error occurred. Please try again later.");
     }
   }
-
 
   return (
     <div className="container-fluid vw4">
@@ -84,7 +78,7 @@ export default function GymLocator({ title, setTitle }) {
           onClick={fetchGyms}
           style={{ width: "10rem" }}
         >
-          search
+          Search
         </button>
       </div>
 
@@ -100,8 +94,9 @@ export default function GymLocator({ title, setTitle }) {
                     <Card.Title>{gym.properties.name}</Card.Title>
                     <Card.Text>{gym.properties.address_line2}</Card.Text>
                     <Card.Text>
-                      Aproximately {(gym.properties.distance / 1000).toFixed(2)}
-                      km away from the location specified
+                      Approximately{" "}
+                      {(gym.properties.distance / 1000).toFixed(2)}
+                      km away from the specified location
                     </Card.Text>
 
                     <button
